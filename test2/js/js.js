ac71f10c356829c4815cff34f7d6fa0b4bb4251c
Josue <victorjosue1260@gmail.com>
2018-04-02 16:36:01 -0600

    actualizacion de procedimiento obtener departamentos

--- test2/js/js.js
+++ test2/js/js.js
@@ -36,32 +36,16 @@ function EnviarMensajeDinamico() {
     //Callback que carga las carreras a la pagina 
     obtenerDepartamentos(
         function carrerasDinamicas() {
-           texto = '<br\> <input id="ALL" type="button" onclick="marcarTODOS()" class="btn btn-primary btn-block btn-large" value="Marcar todos"/> <br\><p>';
-           for (i = 0; i < listaDepartamentos.length; i++) {
-               texto += '<div class="bg" id="' + i + '" style="visibility:hidden"> <div class="cosa1">' + listaDepartamentos[i].nombre + ' - ' + dep[i] + ':</div> <div class="cosa2"><input id="' + dep[i] + '" value="' + listaDepartamentos[i].nombre + '" class="right" type="checkbox" data-off-color="warning"/></div></div>';
-            }
-<<<<<<< HEAD
-            texto += '</p>';
-        var client = new XMLHttpRequest();
-        client.open('GET', '/infoTec/js/EnviarMensaje.html');
-        client.onreadystatechange = function () {
-            document.getElementById('MainDiv').innerHTML = client.responseText;
-            document.getElementById('carreraDiv').innerHTML = texto;
-            document.getElementById("sedeMensaje").addEventListener("change", cargarSedes);
-            
+            texto = '<br\> <input id="ALL" type="button" onclick="marcarTODOS()" class="btn btn-primary btn-block btn-large" value="Marcar todos"/> <br\><p>';
             for (i = 0; i < listaDepartamentos.length; i++) {
-                $("#" + listaDepartamentos[i].codigoDep).bootstrapSwitch();
+                texto += '<div class="bg" id="' + i + '" style="visibility:hidden"> <div class="cosa1">' + listaDepartamentos[i].nombre + ' - ' + dep[i] + ':</div> <div class="cosa2"><input id="' + dep[i] + '" value="' + listaDepartamentos[i].nombre + '" class="right" type="checkbox" data-off-color="warning"/></div></div>';
             }
-            $("#switch-offColor").bootstrapSwitch();
-            $("#datepicker").datepicker();
-=======
-           texto += '</p>';
-            
+            texto += '</p>';
+
             var client = new XMLHttpRequest();
             client.open('GET', '/infoTec/js/EnviarMensaje.html');
 
             client.onreadystatechange = function () {
-<<<<<<< HEAD
                 document.getElementById('MainDiv').innerHTML = client.responseText;
                 document.getElementById('carreraDiv').innerHTML = texto;
                 document.getElementById("sedeMensaje").addEventListener("change", cargarSedes);
@@ -69,25 +53,15 @@ function EnviarMensajeDinamico() {
                 for (a = 0; a < dep.length; a++) {
                     $('#' + dep[a]).bootstrapSwitch();
                 }
-=======
-               document.getElementById('MainDiv').innerHTML = client.responseText;
-               document.getElementById('carreraDiv').innerHTML = texto;
-               document.getElementById("sedeMensaje").addEventListener("change", cargarSedes);
->>>>>>> 4ff69949f92f772e166537cfc00f00e82bd0eeda
-            
-               for (a = 0; a < listaDepartamentos.length; a++) {
-                   $("#" + listaDepartamentos[a].codigoDep).bootstrapSwitch();
-                   console.log(listaDepartamentos[a].codigoDep);
-               }
->>>>>>> dfa8f56eb4c0ff28240a84e2cb5c9ca7e62929b0
 
                 $("#switch-offColor").bootstrapSwitch();
                 $("#datepicker").datepicker()
 
-                };
-            client.send();    
-       });
+            };
+            client.send();
+        });
 }
+
 
 /// <summary>
 /// Carga el html de administracion de departamentos a la pagina
